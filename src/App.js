7490ed038b997055c27995b2d8b5d6db38bff07b
Shloak Gupta <shloakgupta09@gmail.com>
2025-12-30 21:40:20 -0600

    Add Vercel Analytics for web analytics tracking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Header from './components/Header';
 import Home from './components/Home';
 import VideoLibrary from './components/VideoLibrary';
@@ -18,6 +19,7 @@ function App() {
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
+        <Analytics />
       </div>
     </Router>
   );
